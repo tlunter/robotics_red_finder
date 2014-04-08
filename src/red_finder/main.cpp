0a13eeb3b3c30e545e8567a86749afdde50cb6c2
Todd Lunter <tlunter@gmail.com>
2014-04-08 21:37:30 +0000

    Find and report the X and Y means

--- src/red_finder/main.cpp
+++ src/red_finder/main.cpp
@@ -24,6 +24,48 @@ int main(int argc, char **argv)
 
         cv::imshow("Red colors", *clear);
 
+        std::cout << "Rows: " << clear->rows << " Cols: " << clear->cols << std::endl;
+
+        float xmean = 0;
+        float ymean = 0;
+        int points = 0;
+
+        for (int i = 0; i < clear->rows; i++)
+        {
+            for (int j = 0; j < clear->cols; j++)
+            {
+                if (clear->at<uchar>(i,j) > 127)
+                {
+                    xmean += j;
+                    ymean += i;
+                    points += 1;
+                }
+            }
+        }
+
+        if (points > 0)
+        {
+            xmean /= points;
+            ymean /= points;
+        }
+
+        std::cout << "X: " << xmean << " Y: " << ymean << std::endl;
+
+        int midpoint = clear->cols / 2;
+
+        if (xmean < (midpoint - 20))
+        {
+            std::cout << "Left" << std::endl;
+        }
+        else if (xmean > (midpoint + 20))
+        {
+            std::cout << "Right" << std::endl;
+        }
+        else
+        {
+            std::cout << "Center" << std::endl;
+        }
+
         frame->release();
         clear->release();
         if(cv::waitKey(30) == 27) break;
